3084d058f55d7882bd0500dcd970ca33b0dbbc6a
cjpcloud <35867500+cjpcloud@users.noreply.github.com>
2021-04-26 11:45:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,60 +1,7 @@
 
 
 
-      kubernetes_version      = "${var.kubernetes_version}"
-      location                = azurerm_resource_group.MC_KubernetesPreview.location
-      name                    = "managedClusters_gore_k8s_preview_name"
-      resource_group_name     = azurerm_resource_group.MC_KubernetesPreview.name
-
-
-
-      addon_profile {
-
-
-
-
-          http_application_routing {
-              enabled                            = "false"
-
-            }
-
-          kube_dashboard {
-              enabled = "true"
-            }
-
-   #       oms_agent {
-   #          enabled                    = "true"
-   #          log_analytics_workspace_id = "workspaces_kubernetespreview_cca_externalid"
-   #          oms_agent_identity =  {
-   #                         loganalyticsworkspaceresourceid = "[list('workspaces_kubernetespreview_cca_externalid')]"
-   #                     }
-   #
-   #         }
-        }
-
-
-
-      default_node_pool {
-          max_pods             = "30"
-          name                 = "agentpool"
-          node_count           = 12
-          orchestrator_version = "${var.orchestrator_version}"
-          os_disk_size_gb      = "30"
-          type                 = "${var.type}"
-          vm_size              = "30"
-        }
-
-      network_profile {
-          dns_service_ip     = "${var.dns_service_ip}"
-          docker_bridge_cidr = "${var.docker_bridge_cidr}"
-          load_balancer_sku  = "Basic"
-          network_plugin     = "${var.network_plugin}"
-
-          outbound_type      = "${var.outbound_type}"
-
-          service_cidr       = "${var.service_cidr}"
-
-
+      
         }
 
 
